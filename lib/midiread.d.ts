7c44484aa5409190b243ab4dc779a29a8b5abaf0
Yisheng Jiang <yishengjiang@yahoo.com>
2021-07-28 22:29:33 -0700

    new version

---
+++ lib/midiread.d.ts
@@ -0,0 +1,5 @@
+export declare function readMidi(buffer: Uint8Array): {
+    division: number;
+    tracks: any[];
+    ntracks: number;
+};
---
+++ scheduler.ts
@@ -0,0 +1,19 @@
+import {readMidi} from './midiread.js'
+async function schedule(url) {
+
+  const res = await fetch(url);
+  const ab = await res.arrayBuffer();
+
+  const {tracks,division} = readMidi(new Uint8Array(ab));
+	let time=0;
+	setInterval(()=>{
+		tracks.forEach(t=>{
+			if(t && t[0] && t[0][0]< time){
+				const eventt=t.shift();
+				postMessage(eventt);
+			}
+		});
+		time++;
+	},11)
+}
+schedule(self.location.hash.split("#")[1]);
